46367ca08d9e079e2523a6c1955c47ebbfe6d474
lahdesmakilauri@gmail.com <lahdesmakilauri@gmail.com>
2023-05-10 13:47:07 +0300

    connectionFix

--- dbconnection.php
+++ dbconnection.php
@@ -1,8 +1,7 @@
 <?php
 
 function createDbConnection() {
-    
-    $ini = parse_ini_file("myconf.ini");
+    $ini = parse_ini_file("./myconf.ini");
 
     $host = $ini["host"];
     $db = $ini["db"];
